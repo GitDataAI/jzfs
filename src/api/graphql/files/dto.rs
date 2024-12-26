222dc227bb4651c780063dd99802657819c2abd1
ZhenYi <434836402@qq.com>
2024-12-26 11:26:55 +0800

    Optimize file block transfer

--- src/api/graphql/files/dto.rs
+++ src/api/graphql/files/dto.rs
@@ -8,4 +8,14 @@ pub struct GraphQLFileDto{
     pub hash: Option<String>,
     pub branch: String,
     pub path: String,
+    pub size_limit: i32,
+    pub block: i32,
+}
+
+#[derive(Deserialize,Serialize,ToSchema)]
+pub struct GraphQLFileOV{
+    pub total: i32,
+    pub current: i32,
+    pub size: i32,
+    pub data: Vec<u8>
 }
--- src/api/graphql/files/handler.rs
+++ src/api/graphql/files/handler.rs
@@ -1,5 +1,5 @@
 use crate::api::app_write::AppWrite;
-use crate::api::graphql::files::dto::GraphQLFileDto;
+use crate::api::graphql::files::dto::{GraphQLFileDto, GraphQLFileOV};
 use crate::metadata::service::MetaService;
 use actix_web::{web, Responder};
 
@@ -22,11 +22,28 @@ pub async fn graphql_files_handler(
 {
     let repo = match service.repo_service().owner_name_by_model(dto.owner.clone(), dto.repo.clone()).await{
         Ok(uid) => uid,
-        Err(e) => return AppWrite::<Vec<u8>>::fail(e.to_string())
+        Err(e) => return AppWrite::<GraphQLFileOV>::fail(e.to_string())
     };
     let tree = match service.repo_service().get_file(repo.uid, dto.branch.clone(), dto.hash.clone(),dto.path.clone()).await{
         Ok(tree) => tree,
-        Err(e) => return AppWrite::<Vec<u8>>::fail(e.to_string())
+        Err(e) => return AppWrite::<GraphQLFileOV>::fail(e.to_string())
     };
-    AppWrite::<Vec<u8>>::ok(tree)
+    if dto.block < 0{
+        AppWrite::<GraphQLFileOV>::ok(GraphQLFileOV{
+            total: 1,
+            current: dto.block,
+            size: dto.size_limit,
+            data: tree,
+        })
+    }else {
+        let result = tree[(dto.block * dto.size_limit) as usize..((dto.block + 1) * dto.size_limit) as usize].to_vec();
+        AppWrite::<GraphQLFileOV>::ok(GraphQLFileOV{
+            total: tree.len() as i32 / dto.size_limit,
+            current: dto.block,
+            size: dto.size_limit,
+            data: result,
+        })
+    }
+ 
 }
+
