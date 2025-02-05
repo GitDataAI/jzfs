8c0df967b0a3e07ff0b81b7b2cf991f30864b399
ZhenYi <434836402@qq.com>
2025-02-06 01:40:44 +0800

    feat: Add MKD Kafka topic producer client

--- lib-mq/src/client/client.rs
+++ lib-mq/src/client/client.rs
@@ -1,55 +1,46 @@
-// use std::sync::{Arc, Mutex};
-// use log::info;
-// use tokio_nsq::{NSQChannel, NSQProducer, NSQTopic};
-// use lib_config::AppNacos;
-// use rand::{Rng};
-// 
-// pub struct AppNQLClient {
-//     producer: Arc<Mutex<NSQProducer>>,
-//     topic: Arc<NSQTopic>,
-//     _channel: Arc<NSQChannel>
-// }
-// 
-// impl AppNQLClient {
-//     pub async fn init(nacos: AppNacos, topic: String, channel: String) -> std::io::Result<Self>{
-//         let config = nacos.config;
-//         let nsq_config = config.nsq_config().await?;
-//         let mut address = Vec::new();
-//         for addr in nsq_config.addrs {
-//             address.push(addr);
-//         }
-//         let tc = NSQTopic::new(&topic).unwrap();
-//         let ch = NSQChannel::new(&channel).unwrap();
-//         info!("nsq server init topic: {} channel: {}", topic, channel);
-//         let mut rng = rand::thread_rng();
-//         let addr = address[rng.gen_range(0, address.len())].clone();
-//         let consumer = tokio_nsq::NSQProducerConfig::new(
-//             addr
-//         )
-//             .build();
-//         Ok(Self {
-//             producer: Arc::new(Mutex::new(consumer)),
-//             topic: tc,
-//             _channel: ch
-//         })
-//     }
-//     pub async fn send(&self, message: Vec<u8>) -> std::io::Result<()>{
-//         self.producer.lock().unwrap().publish(&self.topic, message).await.ok();
-//         Ok(())
-//     }
-// }
-// 
-// #[cfg(test)]
-// mod tests {
-//     use lib_config::AppNacos;
-//     use crate::{CHANNEL, EMAIL_TOPIC};
-//     use crate::server::email::EmailType;
-//     use super::*;
-//     #[tokio::test]
-//     async fn test_nql_client() {
-//         let nacos = AppNacos::from_env().unwrap(); 
-//         let client = AppNQLClient::init(nacos, EMAIL_TOPIC.to_string(), CHANNEL.to_string()).await.unwrap();
-//         client.send(serde_json::to_vec(&EmailType::Captcha).expect("REASON")).await.unwrap();
-//         tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-//     }
-// }
+use std::time::Duration;
+use log::{info, warn};
+use rdkafka::ClientConfig;
+use lib_config::AppNacos;
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::util::Timeout;
+use uuid::Uuid;
+
+#[derive(Clone)]
+pub struct AppKafkaClient {
+    producer: FutureProducer,
+}
+
+impl AppKafkaClient {
+    pub async fn init(nacos: AppNacos, group: String) -> std::io::Result<Self>{
+        let config = nacos.config.kafka_config().await.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?
+            .addrs.first()
+            .map(|s| s.to_string())
+            .ok_or(std::io::Error::new(std::io::ErrorKind::Other, "kafka config error"))?;
+        let producer:FutureProducer = ClientConfig::new()
+            .set("bootstrap.servers", config)
+            .set("group.id", &group)
+            .set("session.timeout.ms", "6000")
+            .set("enable.auto.commit", "false")
+            .set("auto.offset.reset", "earliest")
+            .create()
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        info!("kafka producer init success");
+        Ok(AppKafkaClient {
+            producer
+        })
+    }
+    pub async fn send(&self, topic: String, key: Option<String> ,message: Vec<u8>) -> std::io::Result<()>{
+        let key = key.unwrap_or(Uuid::new_v4().to_string());
+        let record = FutureRecord::to(&topic)
+            .key(key.as_bytes())
+            .payload(&message);
+        self.producer.send(record, Timeout::After(Duration::from_secs(60))).await
+            .map_err(|e| {
+                warn!("kafka send error:{}", e.0.to_string());
+                std::io::Error::new(std::io::ErrorKind::Other, e.0.to_string())
+            })?;
+        Ok(())
+    }
+}
+
