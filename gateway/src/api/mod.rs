53f2c640ff33eb0b7ebbf4f3e1bd9aa7aed5861a
ZhenYi <434836402@qq.com>
2025-02-07 09:43:30 +0800

    feat: update indentation and formatting settings

--- gateway/src/api/mod.rs
+++ gateway/src/api/mod.rs
@@ -16,6 +16,8 @@ pub async fn endpoint(request: HttpRequest, data: web::Data<Vec<HttpServiceNode>
     if let Ok(Some(x)) = session.get::<String>("index"){
         println!("{}", x);
     }
+    let method = request.method();
+    let header = request.headers();
     if url.starts_with("/api/"){
         let url = url.split("/").collect::<Vec<&str>>();
         if url.len() < 3 {
@@ -28,9 +30,11 @@ pub async fn endpoint(request: HttpRequest, data: web::Data<Vec<HttpServiceNode>
         // };
         let service = url[3];
         let path = url[3..].join("/");
-        let method = request.method();
-        let header = request.headers();
+
         if let Some(server) = data.iter().find(|x| x.endpoint == service) {
+            if server.ips.is_empty() {
+                return HttpResponse::ServiceUnavailable().body("503 Service Unavailable");
+            }
             let host = format!("http://{}:{}/{}", server.ips[0], server.port, path);
             let client = awc::Client::new();
             let mut req = client.request(method.clone(), host);
@@ -48,6 +52,36 @@ pub async fn endpoint(request: HttpRequest, data: web::Data<Vec<HttpServiceNode>
                 response.append_header((key.clone(), value.clone()));
             }
             return response.body(res.body().await.unwrap_or(Bytes::new()));
+        }
+    }
+    if let web_path = data.iter().filter(|x| x.endpoint.starts_with("path@")).map(|x|x.clone()).collect::<Vec<HttpServiceNode>>(){
+        for x in web_path.iter(){
+            let endpoint = x.endpoint.split("@").collect::<Vec<&str>>()
+                .last()
+                .map(|x| x.to_string())
+                .unwrap_or("".to_string());
+            if url.starts_with(&endpoint){
+                if x.ips.is_empty() {
+                    return HttpResponse::ServiceUnavailable().body("503 Service Unavailable");
+                }
+                let host = format!("http://{}:{}/{}", x.ips[0], x.port, url);
+                let client = awc::Client::new();
+                let mut req = client.request(method.clone(), host);
+                for (key, value) in header.iter() {
+                    req = req.append_header((key.clone(), value.clone()));
+                }
+                let req = req.send_stream(payload)
+                    .await;
+                let mut res = match req {
+                    Ok(res) => res,
+                    Err(_) => return HttpResponse::ServiceUnavailable().body("503 Reverse proxy service error"),
+                };
+                let mut response =  HttpResponse::build(res.status());
+                for (key, value) in res.headers() {
+                    response.append_header((key.clone(), value.clone()));
+                }
+                return response.body(res.body().await.unwrap_or(Bytes::new()));
+            }
         }
     }
     HttpResponse::NotFound().body("404 notfound")
