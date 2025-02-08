ec34e423d85f0f3397f1ac2c84171ef4c7c3d9c8
ZhenYi <434836402@qq.com>
2025-02-08 14:23:31 +0800

    refactor: Improve gateway/src/api/mod.rs performance and reverse proxy functionality

--- gateway/src/api/mod.rs
+++ gateway/src/api/mod.rs
@@ -12,7 +12,8 @@ use lib_config::naming::HttpServiceNode;
  */
 pub async fn endpoint(request: HttpRequest, data: web::Data<Vec<HttpServiceNode>>, payload: Payload, session: Session) -> HttpResponse {
     let url = request.uri();
-    let url = url.path().to_string();
+    let url = url.to_string();
+    
     if let Ok(Some(x)) = session.get::<String>("index"){
         println!("{}", x);
     }
@@ -23,11 +24,6 @@ pub async fn endpoint(request: HttpRequest, data: web::Data<Vec<HttpServiceNode>
         if url.len() < 3 {
             return HttpResponse::NotFound().body("404 notfound");
         }
-        // 暂时不求管她
-        // let _version = match ApiVersion::try_from(url[1]) {
-        //     Ok(version) => version,
-        //     Err(_) => return HttpResponse::NotFound().body("404 notfound"),
-        // };
         let service = url[3];
         let path = url[3..].join("/");
 
@@ -56,33 +52,56 @@ pub async fn endpoint(request: HttpRequest, data: web::Data<Vec<HttpServiceNode>
     }
     let web_path = data.iter().filter(|x| x.endpoint.starts_with("path@")).map(|x|x.clone()).collect::<Vec<HttpServiceNode>>();
     {
+        let urls = url.clone()
+            .split("/")
+            .map(|x|x.to_string())
+            .collect::<Vec<_>>();
+        let mut server:Option<HttpServiceNode> = None;
+        let mut length = 0;
         for x in web_path.iter(){
             let endpoint = x.endpoint.split("@").collect::<Vec<&str>>()
                 .last()
                 .map(|x| x.to_string())
                 .unwrap_or("".to_string());
-            if url.starts_with(&endpoint){
-                if x.ips.is_empty() {
-                    return HttpResponse::ServiceUnavailable().body("503 Service Unavailable");
+            let endpoint_urls = endpoint.split("/").map(|x|x.to_string()).collect::<Vec<_>>();
+            if endpoint_urls.len() > length {
+                let mut flag = true;
+                for (i, url) in urls.iter().enumerate() {
+                    if i >= endpoint_urls.len() {
+                        break;
+                    }
+                    if url != &endpoint_urls[i] {
+                        flag = false;
+                        break;
+                    }
                 }
-                let host = format!("http://{}:{}/{}", x.ips[0], x.port, url);
-                let client = awc::Client::new();
-                let mut req = client.request(method.clone(), host);
-                for (key, value) in header.iter() {
-                    req = req.append_header((key.clone(), value.clone()));
+                if flag {
+                    server = Some(x.clone());
+                    length = endpoint_urls.len();
                 }
-                let req = req.send_stream(payload)
-                    .await;
-                let mut res = match req {
-                    Ok(res) => res,
-                    Err(_) => return HttpResponse::ServiceUnavailable().body("503 Reverse proxy service error"),
-                };
-                let mut response =  HttpResponse::build(res.status());
-                for (key, value) in res.headers() {
-                    response.append_header((key.clone(), value.clone()));
-                }
-                return response.body(res.body().await.unwrap_or(Bytes::new()));
             }
+        }
+        if let Some(x) = server {
+            if x.ips.is_empty() {
+                return HttpResponse::ServiceUnavailable().body("503 Service Unavailable");
+            }
+            let host = format!("http://{}:{}/{}", x.ips[0], x.port, url);
+            let client = awc::Client::new();
+            let mut req = client.request(method.clone(), host);
+            for (key, value) in header.iter() {
+                req = req.append_header((key.clone(), value.clone()));
+            }
+            let req = req.send_stream(payload)
+                .await;
+            let mut res = match req {
+                Ok(res) => res,
+                Err(_) => return HttpResponse::ServiceUnavailable().body("503 Reverse proxy service error"),
+            };
+            let mut response =  HttpResponse::build(res.status());
+            for (key, value) in res.headers() {
+                response.append_header((key.clone(), value.clone()));
+            }
+            return response.body(res.body().await.unwrap_or(Bytes::new()));
         }
     }
     HttpResponse::NotFound().body("404 notfound")
