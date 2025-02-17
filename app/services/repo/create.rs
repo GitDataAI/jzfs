485a7abb9693eb74411f575348543a4ecb23ce36
ZhenYi <434836402@qq.com>
2025-02-18 04:57:11 +0800

    feat: add support for creating new repositories with default branch

--- app/services/repo/create.rs
+++ app/services/repo/create.rs
@@ -1,10 +1,11 @@
-use sea_orm::{ActiveModelTrait, QueryFilter, Set};
+use sea_orm::{ActiveModelTrait, QueryFilter, Set, TransactionTrait, TryIntoModel};
 use sea_orm::ColumnTrait;
 use std::io;
 use std::io::Write;
-use git2::Signature;
+use git2::{ Signature};
 use sea_orm::EntityTrait;
 use serde::{Deserialize, Serialize};
+use tracing::info;
 use uuid::Uuid;
 use crate::app::http::GIT_ROOT;
 use crate::app::services::AppState;
@@ -34,6 +35,8 @@ impl AppState {
         if !self.check_repo_name(users_uid, parma.name.clone()).await? {
             return Err(io::Error::new(io::ErrorKind::Other, "repository name already exists"));
         }
+        let txn = self.write.begin().await
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
         let action = repository::ActiveModel {
             uid: Set(Uuid::new_v4()),
             node_uid: Set(Uuid::nil()),
@@ -53,24 +56,27 @@ impl AppState {
             description: Set(parma.description),
             visibility: Set(parma.private),
             fork: Set(None),
-            default_branch: Set(parma.default_branch),
+            default_branch: Set(parma.default_branch.clone()),
             created_at: Set(chrono::Local::now().naive_local()),
             updated_at: Set(chrono::Local::now().naive_local()),
             http: Set("".to_string()),
             created_by: Set(users_uid),
         };
-        let repo = action.insert(&self.write)
+        action.clone().insert(&txn)
             .await
             .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        let repo = action.try_into_model().unwrap();
         let path = format!("{}/{}/{}", GIT_ROOT, repo.node_uid, repo.uid);
         let par = format!("{}/{}", GIT_ROOT, repo.node_uid);
         if !std::path::Path::new(&par).exists() {
             std::fs::create_dir_all(&par)
                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
         }
-        git2::Repository::init(&path)
+        let repo = git2::Repository::init_bare(&path)
             .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-        
+        repo.set_head(&format!("refs/heads/{}", parma.default_branch)).map_err(
+            |e| io::Error::new(io::ErrorKind::Other, e)
+        )?;
         if parma.readme {
             let users = users::Entity::find()
                 .filter(users::Column::Uid.eq(users_uid))
@@ -78,22 +84,28 @@ impl AppState {
                 .await
                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?
                 .ok_or(io::Error::new(io::ErrorKind::Other, "user not found"))?;
-            
+            let tmp = tempfile::tempdir().map_err(|e|{
+                io::Error::new(io::ErrorKind::Other, e)
+            })?;
+            let repo = git2::Repository::clone(&path, tmp.path())
+                .map_err(|e| {
+                    io::Error::new(io::ErrorKind::Other, e)
+                })?;
+            repo.set_head(&format!("refs/heads/{}", parma.default_branch)).map_err(
+                |e| io::Error::new(io::ErrorKind::Other, e)
+            )?;
             let time = chrono::Local::now().naive_local();
             let time = git2::Time::new(time.and_utc().timestamp(), time.and_utc().timestamp_subsec_nanos() as i32);
             let sig = Signature::new(&users.name, &users.email,&time)
                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-            
-            let mut file = std::fs::File::create(format!("{}/README.md", path))
+
+            let mut file = std::fs::File::create(tmp.path().join("README.md"))
                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
             
             file.write_all(b"# README")
                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
             
             file.flush()
-                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-
-            let repo = git2::Repository::open(&path)
                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
             
             git2::Repository::index(&repo)
@@ -114,7 +126,7 @@ impl AppState {
             let tree = repo.find_tree(tree_oid)
                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
 
-            git2::Repository::commit(
+            let commit_oid = git2::Repository::commit(
                 &repo,
                 None,
                 &sig,
@@ -124,7 +136,31 @@ impl AppState {
                 &[],
             )
             .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+            git2::Repository::branch(
+                &repo,
+                &parma.default_branch,
+                &repo.find_commit(commit_oid).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?,
+                false,
+            )
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+            let mut remote = repo.find_remote("origin")
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+            let mut callbacks = git2::RemoteCallbacks::new();
+            callbacks.push_update_reference(|_,status|{
+                if status.is_some() {
+                    info!("Push failed");
+                    return Err(git2::Error::from_str("Failed to push"));
+                }
+                info!("Push success");
+                Ok(())
+            });
+            let mut push_options = git2::PushOptions::new();
+            push_options.remote_callbacks(callbacks);
+            remote.push(&[format!("refs/heads/{}", &parma.default_branch)], Some(&mut push_options))
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+            tmp.close().ok();
         }
+        txn.commit().await.map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
         Ok(())
     }
 }
