b7aeaeddeaa696bd8c6545f208100aa6889c9753
ZhenYi <434836402@qq.com>
2025-02-19 01:01:19 +0800

    feat: Remove unused auth routes

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,4 @@
 import {Route, Routes} from "react-router-dom";
-import AuthLayout from "@/app/auth/layout.tsx";
-import Apply from "@/app/auth/Apply.tsx";
-import Reset from "@/app/auth/Reset.tsx";
-import Login from "@/app/auth/Login.tsx";
 import RootLayout from "@/app/Layout.tsx";
 import LayoutUser from "@/app/user/layout.tsx";
 import RepoLayout from "@/app/repo/layout.tsx";
@@ -10,11 +6,6 @@ import RepoLayout from "@/app/repo/layout.tsx";
 function App() {
     return (
         <Routes>
-            <Route path={"/auth"} element={<AuthLayout/>}>
-                <Route path={"login"} element={<Login/>}/>
-                <Route path={"reset"} element={<Reset/>}/>
-                <Route path={"apply"} element={<Apply/>}/>
-            </Route>
             <Route path={"/"} element={<RootLayout/>}>
                 <Route path={":username"} element={<LayoutUser/>}>
                 </Route>
