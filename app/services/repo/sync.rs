9dd1e55c183bef125f6a75640a584dac039c2ffd
ZhenYi <434836402@qq.com>
2025-02-19 22:49:49 +0800

    feat: Update repo sync behavior to set default branch and updated date

--- app/services/repo/sync.rs
+++ app/services/repo/sync.rs
@@ -1,9 +1,10 @@
 use crate::app::services::AppState;
 use crate::model::repository::tree;
 use sea_orm::ActiveValue::Set;
-use sea_orm::ColumnTrait;
+use sea_orm::{ColumnTrait, IntoActiveModel};
 use sea_orm::{ActiveModelTrait, EntityTrait, QueryFilter};
 use std::io;
+use chrono::{DateTime, Utc};
 use uuid::Uuid;
 
 impl AppState {
@@ -13,7 +14,43 @@ impl AppState {
         let blob = crate::blob::GitBlob::new(path.into())
             .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;     
         let branch = blob.branch()?;
-        for branch in branch {
+        
+        let mut arch = repo.clone().into_active_model();
+        arch.nums_branch = Set(branch.len() as i32);
+        if branch.iter().any(|x|x.name != repo.default_branch) {
+            if branch.len() == 1 {
+                arch.default_branch = Set(branch[0].name.clone());
+                arch.updated_at = Set(chrono::Local::now().naive_local());
+                arch.update(&self.write).await.ok();
+            } else { 
+                // find main or master or first branch
+                if branch.iter().any(|x|x.name == "main" || x.name == "master") {
+                    arch.default_branch = Set(branch[0].name.clone());
+                    arch.updated_at = Set(chrono::Local::now().naive_local());
+                    arch.update(&self.write).await.ok();
+                }else if branch.len() == 1 {
+                    if let Some(first) = branch.first() {
+                        arch.default_branch = Set(first.name.clone());
+                        arch.updated_at = Set(chrono::Local::now().naive_local());
+                        arch.update(&self.write).await.ok();
+                    }
+                }
+                
+            }
+        }
+        let mut rec = 0;
+        let mut commit_len = 0;
+        let branch = blob.blob()?;
+        for (branch, commits) in branch {
+            if let Ok(time) = branch.time.parse::<i64>(){
+                if time > rec {
+                    rec = time;
+                }
+            }
+            if commits.len() > commit_len { 
+                commit_len = commits.len();
+            }
+            
             if tree::Entity::find()
                 .filter(tree::Column::RepoUid.eq(repo_uid))
                 .filter(tree::Column::Branch.eq(branch.name.clone()))
@@ -39,6 +76,17 @@ impl AppState {
                 branch: Set(branch.name),
             }
                 .insert(&self.write).await;
+            
+        }
+        if commit_len > 0 {
+            let mut arch = repo.clone().into_active_model();
+            arch.nums_commit = Set(commit_len as i32);
+            arch.update(&self.write).await.ok();
+        }
+        if rec != 0 {
+            let mut arch = repo.clone().into_active_model();
+            arch.updated_at = Set(DateTime::from_timestamp(rec, 0).unwrap().naive_local());
+            arch.update(&self.write).await.ok();
         }
         Ok(())
     }
