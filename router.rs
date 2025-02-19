fad7f3aa2cdee6d3b410782fcd3ef6bff7a37e33
ZhenYi <434836402@qq.com>
2025-02-19 22:52:37 +0800

    feat: Add new repository actions to router

--- router.rs
+++ router.rs
@@ -1,6 +1,6 @@
 use poem::{get, patch, post, Route};
 use crate::app::api::handler::auth::{auth_apply, auth_captcha, auth_check, auth_email_check, auth_email_send, auth_logout, auth_passwd};
-use crate::app::api::handler::repo::{repo_bhct, repo_create, repo_file, repo_info, repo_tree};
+use crate::app::api::handler::repo::{repo_bhct, repo_create, repo_file, repo_fork, repo_info, repo_star, repo_tree, repo_watch};
 use crate::app::api::handler::static_file::{down_avatar, upload_avatar};
 use crate::app::api::handler::users::{user_dashbored, user_now, user_update_optional};
 use crate::app::http::git_router;
@@ -42,6 +42,9 @@ pub fn router() -> Route {
                         .at("/:owner/:repo/bhct", post(repo_bhct))
                         .at("/:owner/:repo/info", post(repo_info))
                         .at("/:owner/:repo/branch/:branch/:head/tree", post(repo_tree))
+                        .at("/:owner/:repo/fork", post(repo_fork))
+                        .at("/:owner/:repo/star", post(repo_star))
+                        .at("/:owner/:repo/watch/:level", post(repo_watch))
                 )
         )
        
