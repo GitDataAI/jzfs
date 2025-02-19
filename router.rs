bed767c502d6f8dfe1076f42757dd009979d9c10
ZhenYi <434836402@qq.com>
2025-02-20 01:24:16 +0800

    feat: Add access endpoint for repositories

--- router.rs
+++ router.rs
@@ -1,6 +1,6 @@
 use poem::{get, patch, post, Route};
 use crate::app::api::handler::auth::{auth_apply, auth_captcha, auth_check, auth_email_check, auth_email_send, auth_logout, auth_passwd};
-use crate::app::api::handler::repo::{repo_bhct, repo_create, repo_file, repo_fork, repo_info, repo_star, repo_tree, repo_watch};
+use crate::app::api::handler::repo::{repo_access, repo_bhct, repo_create, repo_file, repo_fork, repo_info, repo_star, repo_tree, repo_watch};
 use crate::app::api::handler::static_file::{down_avatar, upload_avatar};
 use crate::app::api::handler::users::{user_dashbored, user_now, user_update_optional};
 use crate::app::http::git_router;
@@ -39,6 +39,7 @@ pub fn router() -> Route {
                     Route::new()
                         .at("/file", post(repo_file))
                         .at("/create", post(repo_create))
+                        .at("/access",get(repo_access))
                         .at("/:owner/:repo/bhct", post(repo_bhct))
                         .at("/:owner/:repo/info", post(repo_info))
                         .at("/:owner/:repo/branch/:branch/:head/tree", post(repo_tree))
