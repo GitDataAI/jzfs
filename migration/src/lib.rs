8ef0dc419f8d923990e5bdb86d2b149fff20e6a2
touyoutaaa <1849625991@qq.com>
2025-07-11 18:50:56 +0800

    feat(migration):change migration table

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -9,7 +9,6 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(migra::m20250706_000001::Migration),
              Box::new(stable::Migration)
         ]
     }
--- migration/src/migra/mod.rs
+++ migration/src/migra/mod.rs
@@ -1,4 +1,5 @@
 pub mod m20250706_000001;
 pub mod m20250707_000001;
+pub mod m20250711_000001;
 
-pub use m20250707_000001 as stable;
+pub use m20250711_000001 as stable;
